df3d4f4c050c918c932e7dd813a1693ba2cc7849
Saptarshe Sinha <128472537+Im-Sapto@users.noreply.github.com>
2023-09-01 20:50:15 +0530

    Updates-2
    Fixed some mobile responsiveness & tried to fix the font page issues

--- script.js
+++ script.js
@@ -44,26 +44,26 @@ gsap.to("#page>video",{
     }
 })
 
-gsap.to("#page",{
-    scrollTrigger:{
-        trigger:`#page`,
-        start:`top top`,
-        end:`bottom top`,
-        scroller:`#main`,
-        pin:true
-    }
-})
+// gsap.to("#page",{
+//     scrollTrigger:{
+//         trigger:`#page`,
+//         start:`top top`,
+//         end:`bottom top`,
+//         scroller:`#main`,
+//         pin:true
+//     }
+// })
 
-gsap.to("#page-bottom",{
-    scrollTrigger:{
-        trigger:`#page-bottom`,
-        start:`5% top`,
-        end:`bottom top`,
-        scroller:`#main`,
-        // scrub:.5,
-    },
-    opacity:0
-})
+// gsap.to("#page-bottom",{
+//     scrollTrigger:{
+//         trigger:`#page-bottom`,
+//         start:`5% top`,
+//         end:`bottom top`,
+//         scroller:`#main`,
+//         // scrub:.5,
+//     },
+//     opacity:0
+// })
 
 var tl = gsap.timeline({
     scrollTrigger:{
